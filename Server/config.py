f0afa2b98af99ccb6e851a7b174c3d0f46f345a0
Z-Link-C <zachery.croft@gmail.com>
2026-06-13 17:57:47 -0400

    reformatted user.email!=admin_email to be and nots, and removed db url from config

--- Server/config.py
+++ Server/config.py
@@ -10,13 +10,13 @@ from sqlalchemy import MetaData
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 
-DATABASE_URL = "postgresql://neondb_owner:npg_zSUME9sy3cba@ep-purple-butterfly-adsc9tuk-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-if not DATABASE_URL:
+DB_URL = os.environ.get("DB")
+if not DB_URL:
     raise RuntimeError(
         "DATABASE_URL is not set. Run: DATABASE_URL=postgresql://user:pass@localhost/questlog python app.py"
     )
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
